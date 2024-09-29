12f1697659b9f322aabe62584aad8f2ca9075cbe
Dhruv Singh <161761171+Dhruv2816@users.noreply.github.com>
2024-09-29 12:00:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import numpy as np
+timport numpy as np
 import pandas as pd
 import yfinance as yf
 from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
-
+import tensorflow as tf
 model = load_model(r'MODEL/Stock Predictions Model.keras')
 st.header('Stock price predictor')
 
